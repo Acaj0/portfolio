61dd692e22afd0de65d3a19658f210848522f280
Acaj0 <antoniocarlosjr007@hotmail.com>
2025-07-09 14:00:00 -0400

    feat: implementa módulo de autenticação inicial

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           
+          
           {children}
         </ThemeProvider>
       </body>
